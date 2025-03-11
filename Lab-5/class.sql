03d5b158a5a3bbe94aecd9c3b5ba7b739610899c
safi-io <m.safi.ullah@outlook.com>
2025-03-11 13:52:39 +0500

    Group BY, Having Clauses

---
+++ Lab-5/class.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS lab_5;
+USE lab_5;
+
+CREATE TABLE teachers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10,2)
+);
+
+-- Aggregation Functions
+SELECT min(salary) AS minimum_salary_from_teachers FROM teachers;
+SELECT max(salary) AS maximum_salary_from_teachers FROM teachers;
+SELECT count(distinct id) AS total_teachers FROM teachers;
+SELECT sum(salary) AS total_salary FROM teachers;
+SELECT avg(salary) AS avearge_salary FROM teachers;
+
+-- Group By
+SELECT department, count(id)
+FROM teachers
+GROUP BY department;
+
+SELECT department, AVG(salary)
+FROM teachers
+GROUP BY department
+HAVING AVG(salary) < 70000;
